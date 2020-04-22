aab0fed43031fdda9d1b39f7e61ba22567345794
marius220699 <marius.schairer@hfg-gmuend.de>
2020-04-22 20:27:33 +0200

    package jason

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const axios = require('axios').default;
 axios({
   method: 'GET',
   url: 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json'
+  
   responseType: 'json',
 })
 .then((response)=>{
