f8976b7e1eaa7691cb4e1f40b40a777f46345553
marius220699 <marius.schairer@hfg-gmuend.de>
2020-05-02 17:34:34 +0200

    api sketching

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const axios = require('axios').default;
+const express = require('express');
+
+const app = express();
+
+let output ='';
+
 axios({
   method: 'GET',
   url: 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json'
@@ -8,4 +14,21 @@ axios({
 .then((response)=>{
   const result = response.data;
   console.log(result);
+});
+
+app.get('/user/:uid', (req, res) => {
+  res.send(`User ID is set to ${req.params.uid}`);
+  });
+
+app.get('/mensa/:day', (req, res) => {
+  if (req.params.day === 'Di') {
+    res.send(data);
+  } else {
+    res.status(404).send('404');
+  }  
+});
+
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
 });
