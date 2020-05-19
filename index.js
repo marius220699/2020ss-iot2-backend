0656fb522e1a118c5bad09628d45281ffb86c6cd
marius220699 <marius.schairer@hfg-gmuend.de>
2020-05-19 10:29:54 +0200

    mongoDB

--- index.js
+++ index.js
@@ -2,70 +2,86 @@ const axios = require('axios').default;
 const express = require('express');
 const mongo = require('mongodb');
 const cors = require('cors');
+const url = 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json';
 
 const app = express();
     app.use(cors());
     app.use(express.json());
 
-async function initMongoDB() {
+async function getData() {
    const client = await mongo.connect('mongodb://localhost:27017/mensa')
      // eslint-disable-next-line no-console
      .catch((err) => { console.log(err); });
    const db = await client.db();
-   }
-  initMongoDB();
-
-  let data = '';
-const uri = 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json';
-
-async function getData() {
-  await axios.get(uri)
-    .then((req) => {
-      data = req.data;
-    })
-    .catch(() => {
-      data = undefined;
-    });
+   await axios.get(url)
+    .then(async (response) => {
+      response.data.forEach(async d => {
+        createOrUpdate(d, db);
+      });
+   })
+   .catch(e => {
+    console.log(e);
+  });
 }
+
+async function createOrUpdate(object, db) {
+  const r = await db.collection('essen').findOne({category: object.category, day: object.day});
+
+  if (r) {
+    await db.collection('essen').replaceOne({category: object.category, day: object.day}, object);
+  } else {
+    await db.collection('essen').insertOne(object);
+  }
+}
+
+function valid(input) {
+  return input.hasOwnProperty("day") && input.hasOwnProperty("category") && input.hasOwnProperty("name");
+}
+
 getData();
+setInterval(getData, 5000);
 
-app.get('/mensa/:day', (req, res) => {
-  if (data !== undefined) {
-    switch (req.params.day) {
-      case 'Di':
-        res.send(data);
-        break;
-      default:
-        res.status(404).send('Error: 404');
-        break;
+
+app.get('/api/getData/', async (req, res) => {
+  const client = await mongo.connect("mongodb://localhost:27017/mensa").catch(err => { console.log(err); });
+  const db = await client.db();
+  let data = await db.collection('essen').find().toArray();
+  if (data.length === 0) {
+    res.status(404).send({message: 'No results found UwU'});
+    } else {
+      res.status(200).send(data);
     }
-  } else {
-    res.status(404).send('Error: 404');
+});
+
+app.post('/api/addData/:day', async (req, res) => {
+  const client = await mongo.connect("mongodb://localhost:27017/mensa").catch(err => { console.log(err); });
+  const db = await client.db();
+  try {
+    if (valid(req.body)) {
+      await createOrUpdate(req.body, db);
+      res.status(200).send();
+    } else {
+      res.status(403).send();
+    }
+  } catch {
+    console.log('caught');
+    res.status(403).send();
   }
 });
 
-app.post('/api/addData/', (req, res) => {
-  if (!JSON.stringify(data).includes(JSON.stringify(req.body))) {
-    data.push(req.body);
-    res.status(200).send();
-  } else {
-    res.status(418).send();
-  }
-});
-
-app.get('/api/getData/', (req, res) => {
-  // eslint-disable-next-line no-console
-  console.log('Access');
-  res.status(200).send(data);
+app.get('/api/getData/:day', async (req, res) => {
+  const client = await mongo.connect("mongodb://localhost:27017/mensa").catch(err => { console.log(err); });
+  const db = await client.db();
+  let dayonlyData = await db.collection('essen').find({day: req.params.day}).toArray()
+    if (dayonlyData.length === 0) {
+      res.status(400).send({message: 'No results found UwU'});
+    } else {
+      res.status(200).send(dayonlyData);
+    }
 });
 
 // Server starten
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Example app listening on port 3000!');
-});
-
-
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
 });
