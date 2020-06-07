777e717218f7e20fb7cec417c2d95ac5b64c1d19
marius220699 <marius.schairer@hfg-gmuend.de>
2020-06-07 20:42:56 +0200

    update

--- index.js
+++ index.js
@@ -5,59 +5,75 @@ const cors = require('cors');
 const url = 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json';
 
 const app = express();
-    app.use(cors());
-    app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-    async function getData() {
-      const client = await mongo.connect("mongodb://localhost:27017/mensa").catch(err => { console.log(err); });
-      const db = await client.db();
-      await axios.get(url)
-        .then(async (response) => {
-          response.data.forEach(async d => {
-            createOrUpdate(d, db);
-          });
-        })
-        .catch(e => {
-          console.log(e);
-        });
-    }
+async function start() {
+  const client = await mongo.connect("mongodb://localhost:27017/mensa").catch(err => {
+    console.log(err);
+  });
 
+  const db = await client.db();
 
-    async function createOrUpdate(object, db) {
-      const r = await db.collection('essen').findOne({category: object.category, day: object.day});
-    
-      if (r) {
-        await db.collection('essen').replaceOne({category: object.category, day: object.day}, object);
-      } else {
-        await db.collection('essen').insertOne(object);
-      }
-    }
-
-    function valid(input) {
-      return input.hasOwnProperty("day") && input.hasOwnProperty("category") && input.hasOwnProperty("name");
-    }
+  async function getData(){
+    await axios.get(url)
+    .then(async (response) => {
+      response.data.forEach(async d => {
+        createOrUpdate(d, db);
+      });
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }
   
+
+
+
+async function createOrUpdate(object, db) {
+  const r = await db.collection('essen').findOne({
+    category: object.category,
+    day: object.day
+  });
+
+  if (r) {
+    await db.collection('essen').replaceOne({
+      category: object.category,
+      day: object.day
+    }, object);
+  } else {
+    await db.collection('essen').insertOne(object);
+  }
+}
+
+function valid(input) {
+  return input.hasOwnProperty("day") && input.hasOwnProperty("category") && input.hasOwnProperty("name");
+}
+
 
 
 getData();
 setInterval(getData, 5000);
 
 
-app.get('/api/getData/', async (req, res) => {
-  const client = await mongo.connect("mongodb://localhost:27017/mensa").catch(err => { console.log(err); });
-  const db = await client.db();
-  let data = await db.collection('essen').find().toArray();
-  if (data.length === 0) {
-    res.status(404).send({message: 'No results found UwU'});
-    } else {
-      res.status(200).send(data);
-    }
+app.post('/api/like/:id', async (req, res) => {
+  const _id = '5ebec0b21958f60459917f30';
+
+  const before = await db.collection('essen').findOne({
+    _id
+  })
+  if (!before) return res.status(404).send('Das gibts nicht du Vollidiot');
+
+  await db.collection('essen').replaceOne({
+    _id
+  }, {
+    upvotes: before.upvotes + 1
+  });
+  res.status(200).send();
 });
 
-app.post('/api/addData/:day', async (req, res) => {
-  const client = await mongo.connect("mongodb://localhost:27017/mensa").catch(err => { console.log(err); });
-  const db = await client.db();
-  let getresult = await db.collection('essen').find({name : "Nudelsuppe"}).toArray();
+app.post('/api/addData/', async (req, res) => {
+  
   try {
     if (valid(req.body)) {
       await createOrUpdate(req.body, db);
@@ -71,19 +87,35 @@ app.post('/api/addData/:day', async (req, res) => {
   }
 });
 
-app.get('/api/getData/:day', async (req, res) => {
-  const client = await mongo.connect("mongodb://localhost:27017/mensa").catch(err => { console.log(err); });
-  const db = await client.db();
-  let dayonlyData = await db.collection('essen').find({day: req.params.day}).toArray()
+app.get('/api/getData/', async (req, res) => {
+  let data = await db.collection('essen').find().toArray();
+    if (data.length === 0) {
+      res.status(404).send({
+        message: 'No results found UwU'
+      });
+    } else {
+      res.status(200).send(data);
+    }
+  });
+
+  app.get('/api/getData/:day', async (req, res) => {
+
+    let dayonlyData = await db.collection('essen').find({
+      day: req.params.day
+    }).toArray()
     if (dayonlyData.length === 0) {
-      res.status(400).send({message: 'No results found UwU'});
+      res.status(400).send({
+        message: 'No results found UwU'
+      });
     } else {
       res.status(200).send(dayonlyData);
     }
-});
+  });
 
 // Server starten
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Example app listening on port 3000!');
 });
+
+}
